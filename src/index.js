4b6d8920a84bc90e110b4e4ea866c5d2fb3f6384
Shubham Mehra <mehrashubham18@gmail.com>
2021-08-09 19:10:31 +0530

    Logic implement. Updated footer.

--- src/index.js
+++ src/index.js
@@ -1,18 +1,37 @@
 const dateOfBirth = document.querySelector("#birthdate");
 const luckyNumber = document.querySelector("#lucky-number");
 const checkBtn = document.querySelector("#check-btn");
-const outputDiv = document.querySelector("#outputH3");
+const outputText = document.querySelector("#outputH3");
+const outputDiv = document.querySelector(".result");
+const image = document.querySelector("#image");
 
-checkBtn.addEventListener("click", checkBirthdateIsLucky);
-
-function checkBirthdateIsLucky() {
+function checkBirthdateIsLucky(event) {
+  event.preventDefault();
   const dob = dateOfBirth.value;
   const sum = calculateSumOfBirthdate(dob);
-  console.log(sum);
+  compareValues(sum, luckyNumber.value);
 }
 
 function calculateSumOfBirthdate(birthdate) {
   birthdate = birthdate.replaceAll("-", "");
   let sum = 0;
-  return birthdate;
+
+  for (let i = 0; i < birthdate.length; i++) {
+    sum += Number(birthdate.charAt(i));
+  }
+  return sum;
 }
+
+function compareValues(birthdateSum, luckyValue) {
+  if (birthdateSum % luckyValue === 0) {
+    outputDiv.style.display = "block";
+    outputText.innerText = "Your birthdate is lucky😎🥳.";
+    image.src = "./happy_birthday.svg";
+  } else {
+    outputDiv.style.display = "block";
+    outputText.innerText = "Your is not lucky 🤦‍♂️!..";
+    image.src = "./sad_birthdate.svg";
+  }
+}
+
+checkBtn.addEventListener("click", (e) => checkBirthdateIsLucky(e));
